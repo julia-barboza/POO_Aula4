253e1f59e4eda785677da31fd5e536cda84dc88f
júlia barboza <barboza.juliag@gmail.com>
2025-08-20 14:17:31 -0300

    Update Conta.java

--- Conta.java
+++ Conta.java
@@ -3,10 +3,10 @@ package POO4;
 public class Conta {
 
 	int id;
-    String tipo;
-    double saldo;
+	String tipo;
+	double saldo;
 
-    public int getId() {
+	public int getId() {
 		return id;
 	}
 
@@ -30,17 +30,32 @@ public class Conta {
 		this.saldo = saldo;
 	}
 
-	public void sacar(String valor) {
-        System.out.println("Saque de R$ " + valor + " realizado!");
-    }
+	public void sacar(double valor) {
+		if (valor <= 0) {
+			System.out.println("O valor do saque deve ser maior que zero!");
+		} else if (valor > saldo) {
+			System.out.println("Saldo insuficiente para saque de R$ " + valor);
+		} else {
+			saldo -= valor;
+			System.out.println("Saque de R$ " + valor + " realizado!");
+		}
+	}
 
-    public void depositar(String valor) {
-        System.out.println("Depósito de R$ " + valor + " realizado!");
-    }
-    public void consultarHistórico(String historico) {
-        System.out.println("O seu histório é: " + historico);
-    }
-    public void sair (){
-        System.out.println("Você está saindo da conta...");
-    }
+	public void depositar(double valor) {
+		if (valor <= 0) {
+			System.out.println("O valor do depósito deve ser maior que zero!");
+		} else {
+			saldo += valor;
+			System.out.println("Depósito de R$ " + valor + " realizado!");
+		}
+
+	}
+
+	public void consultarHistórico(String historico) {
+		System.out.println("O seu histório é: " + historico);
+	}
+
+	public void sair() {
+		System.out.println("Você está saindo da conta...");
+	}
 }
